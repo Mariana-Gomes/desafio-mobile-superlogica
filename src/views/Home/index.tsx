fd3a88261ee801ddb720e8ed390a93bc60bebad3
Mariana-Gomes <marianagds.dg@gmail.com>
2022-01-17 23:02:46 -0300

    Fix: Search on favorite tab

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -94,7 +94,7 @@ const Home: React.FC = ({navigation}) => {
   }
 
   const listData = () =>{
-    if(searchText != ""){
+    if(searchText != "" && tabActive === 'All'){
       return searchResult
     }
     
